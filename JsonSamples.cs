036145b57ba44d58f1167ca894e1cae083beb59f
Mohamed Elshawaf <m_shawaf@outlook.com>
2021-08-01 18:04:15 +0200

    leveling

--- JsonSamples.cs
+++ JsonSamples.cs
@@ -6,7 +6,7 @@
     {
         public const string SimpleJson = "{\"Application Name\":\"Json Data Tree Visualizer\",\"Supported types\":4}";
         public const string ComplexJson = "{\"Application Name\":\"JsonDataTreeVisualizer\",\"Hosts\":{\"Identity\":\"http://ipaddress:2900/api/\",\"Member\":\"http://ipaddress:2700/api/\",\"Verifier\":{\"KeyManager\":\"http://ipaddress:2000/api/\",\"JwtKeySettings\":{\"KeyLifetimeDays\":1},\"IsAuthEnabled\":false}},\"IsDevelopment\":true,\"ExpectedRequestsPerSec\":150,\"LinkExpiryMins\":5.5,\"Other\":null}";
-        public const string ComplexMultiLevelJson = "{\"Application Name\":\"JsonDataTreeVisualizer\",\"Hosts\":{\"Identity\":\"http://ipaddress:2900/api/\",\"Member\":\"http://ipaddress:2700/api/\",\"Verifier\":{\"KeyManager\":\"http://ipaddress:2000/api/\",\"JwtKeySettings\":{\"KeyLifetimeDays\":1},\"IsAuthEnabled\":false}},\"RabbitMQ\":{\"Host\":\"http://ipaddress:1567/\",\"Username\":\"guest\",\"Password\":\"guest\"},\"IsDevelopment\":true,\"ExpectedRequestsPerSec\":150,\"LinkExpiryMins\":5.5,\"Other\":null}";
+        public const string ComplexMultiLevelJson = "{\"Application Name\":\"JsonDataTreeVisualizer\",\"Hosts\":{\"Identity\":\"http://ipaddress:2900/api/\",\"Member\":\"http://ipaddress:2700/api/\",\"Verifier\":{\"KeyManager\":\"http://ipaddress:2000/api/\",\"JwtKeySettings\":{\"KeyLifetimeDays\":1},\"IsAuthEnabled\":false}},\"RabbitMQ\":{\"Host\":\"http://ipaddress:1567/\",\"Username\":\"guest\",\"Password\":\"guest\"},\"LinkExpiryMins\":5.5}";
 
         public const string InvalidJson = "";
 
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -32,6 +32,9 @@ namespace JsonDataTreeVisualizer.Pages
         public int Level { get; set; }
         public SmartNode ParentNode { get; set; }
         public List<SmartNode> SubNodes { get; set; } = new List<SmartNode>();
+        public Guid? Id { get; set; }
+        public Guid? WrappingNodeId { get; set; }
+
         public static SmartNode CreateFinalNode(string key, object value, JsonValueKind valueKind, int level)
             => new()
             {
@@ -42,12 +45,24 @@ namespace JsonDataTreeVisualizer.Pages
                 Level = level
             };
 
-        public static SmartNode CreateObjectNode(string key, int level)
+        public static SmartNode CreateObjectNode(string key, int level, Guid? wrappingId)
             => new()
             {
                 Key = key,
-                Level = level
+                Level = level,
+                Id = Guid.NewGuid(),
+                WrappingNodeId = wrappingId
             };
+
+        public SmartNode OrderSubNodes()
+        {
+            SubNodes = SubNodes.OrderBy(x => !x.IsFinal).ToList();
+
+            for (int i = 0; i < SubNodes.Count; i++)
+                SubNodes[i] = SubNodes[i].OrderSubNodes();
+
+            return this;
+        }
     }
 
     [DebuggerDisplay("Key = {Key}, Value = {Value}, Level = {Level}")]
@@ -61,6 +76,9 @@ namespace JsonDataTreeVisualizer.Pages
         public JsonValueKind ValueKind { get; set; }
         public string GroupName { get; set; }
         public SimpleDataNode Parent { get; set; }
+        public string ParentName { get; set; }
+        public Guid? WrappingId { get; set; }
+
         public SimpleDataNode(SmartNode node)
         {
             Key = node.Key;
@@ -85,70 +103,20 @@ namespace JsonDataTreeVisualizer.Pages
 
         public void OnGet()
         {
-            var dic = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(JsonSamples.ComplexMultiLevelJson);
-
-            var unorderedSmartNode = CreateNodeTree(dic, "Configurations");
-            HeadNode = OrderSubNodes(unorderedSmartNode);
-            FlatenNodes(HeadNode, FlattenedNodes, null);
+            var jsonObject = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(JsonSamples.ComplexMultiLevelJson);
+            HeadNode = CreateJsonTree(jsonObject, null);
+            FlattenNodes(HeadNode, FlattenedNodes, null);
             NodeGroups = GroupByLevelOrdered(FlattenedNodes);
         }
 
-        private static List<NodeGroup> GroupByLevelOrdered(List<SimpleDataNode> flattenedNodes)
+        private static SmartNode CreateJsonTree(Dictionary<string, JsonElement> dic, string fallbackObjectName)
         {
-            List<NodeGroup> nodeGroups = new();
-
-            var gr = flattenedNodes.GroupBy(x => new { x.GroupName, x.Level })
-                .ToDictionary(x => new GroupHeader { GroupName = x.Key.GroupName, Level = x.Key.Level }, v => v.ToList())
-                .ToList();
-
-            foreach (var item in gr)
-            {
-                nodeGroups.Add(new NodeGroup { Header = item.Key, Nodes = item.Value });
-            }
-
-            return nodeGroups;
-        }
-
-        private void FlatenNodes(SmartNode node,
-            List<SimpleDataNode> flattened,
-            SmartNode wrappingNode)
-        {
-            if (node.IsFinal)
-            {
-                var newnode = new SimpleDataNode(node)
-                {
-                    Parent = new SimpleDataNode(node.ParentNode),
-                    GroupName = wrappingNode.Key
-                };
-                flattened.Add(newnode);
-            }
-            else
-            {
-                foreach (var subNode in node.SubNodes)
-                {
-                    FlatenNodes(subNode, flattened, node);
-                }
-            }
-        }
-
-        private SmartNode OrderSubNodes(SmartNode node)
-        {
-            node.SubNodes = node.SubNodes.OrderBy(x => !x.IsFinal).ToList();
-
-            for (int i = 0; i < node.SubNodes.Count; i++)
-                node.SubNodes[i] = OrderSubNodes(node.SubNodes[i]);
-
-            return node;
-        }
-
-        private static SmartNode CreateNodeTree(Dictionary<string, JsonElement> dic, string fallbackObjectName)
-        {
-            SmartNode parentNode = SmartNode.CreateObjectNode(fallbackObjectName, 0);
-            NodeCreator(dic, parentNode);
+            SmartNode parentNode = SmartNode.CreateObjectNode(fallbackObjectName, 0, Guid.NewGuid());
+            PropagateNodeTree(dic, parentNode, parentNode.WrappingNodeId);
             return parentNode;
         }
 
-        private static void NodeCreator(Dictionary<string, JsonElement> dic, SmartNode parentNode)
+        private static void PropagateNodeTree(Dictionary<string, JsonElement> dic, SmartNode parentNode, Guid? wrapperId)
         {
             int level = parentNode.Level + 1;
             foreach (var item in dic)
@@ -157,9 +125,10 @@ namespace JsonDataTreeVisualizer.Pages
                 switch (item.Value.ValueKind)
                 {
                     case JsonValueKind.Object:
-                        node = SmartNode.CreateObjectNode(item.Key, level);
+                        node = SmartNode.CreateObjectNode(item.Key, level, parentNode.Id);
                         node.ParentNode = parentNode;
-                        NodeCreator(JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(item.Value.GetRawText()), node);
+                        var subDict = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(item.Value.GetRawText());
+                        PropagateNodeTree(subDict, node, wrapperId);
                         parentNode.SubNodes.Add(node);
                         break;
                     case JsonValueKind.String:
@@ -193,9 +162,55 @@ namespace JsonDataTreeVisualizer.Pages
                     default:
                         break;
                 }
+                if (item.Value.ValueKind != JsonValueKind.Object)
+                    node.WrappingNodeId = parentNode.Id;
+                //else
+                //    node.WrappingNodeId = parentNode.WrappingNodeId;
+
                 node.ParentNode = parentNode;
             }
         }
+
+        private static List<NodeGroup> GroupByLevelOrdered(List<SimpleDataNode> flattenedNodes)
+        {
+            List<NodeGroup> nodeGroups = new();
+
+            var gr = flattenedNodes.GroupBy(x => new { x.GroupName, x.Level })
+                .ToDictionary(x => new GroupHeader { GroupName = x.Key.GroupName, Level = x.Key.Level }, v => v.ToList())
+                .ToList();
+
+            foreach (var item in gr)
+            {
+                nodeGroups.Add(new NodeGroup { Header = item.Key, Nodes = item.Value });
+            }
+
+            return nodeGroups;
+        }
+
+        private void FlattenNodes(SmartNode node,
+            List<SimpleDataNode> flattened,
+            SmartNode wrappingNode)
+        {
+            if (node.IsFinal)
+            {
+                var newnode = new SimpleDataNode(node)
+                {
+                    Parent = new SimpleDataNode(node.ParentNode),
+                    ParentName = node.ParentNode?.ParentNode?.Key,
+                    GroupName = wrappingNode.Key,
+                    WrappingId = node.WrappingNodeId
+                };
+                flattened.Add(newnode);
+            }
+            else
+            {
+                foreach (var subNode in node.SubNodes)
+                {
+                    FlattenNodes(subNode, flattened, node);
+                }
+            }
+        }
+
 
 
         public void OnPost()
@@ -207,24 +222,71 @@ namespace JsonDataTreeVisualizer.Pages
         {
             var descendantsTree = GroupByLevelOrdered(FlattenedNodes);
 
-            var generationsTree = descendantsTree.GroupBy(x => new { x.Header.Level }, x => new { x.Nodes, x.Header.GroupName }).ToList();
+            //var generationsTree = descendantsTree.GroupBy(
+            //    nodeGroup => new { nodeGroup.Header.Level },
+            //    nodeGroup2 => new { nodeGroup2.Nodes, nodeGroup2.Header.GroupName })
+            //        .Select(group =>
+            //    new NodesBatches
+            //    {
+            //        Header = new GroupHeader
+            //        {
+            //            Level = group.Key.Level,
+            //            GroupName = group.FirstOrDefault()?.GroupName
+            //        },
+            //        Nodes = group.Select(x => x.Nodes).ToList()
+            //    }).ToList();
+
+            var generationsTree = descendantsTree.GroupBy(
+                nodeGroup => new { nodeGroup.Header.Level },
+                nodeGroup2 => new { nodeGroup2.Nodes, nodeGroup2.Header.GroupName })
+                    .Select(group =>
+                new NodesBatches
+                {
+                    Header = new GroupHeader
+                    {
+                        Level = group.Key.Level,
+                        GroupName = group.FirstOrDefault()?.GroupName,
+                    },
+                    NodeGroups = group.Select(x => x.Nodes).ToList()
+                }).ToList();
 
             var topJson = new Dictionary<string, object>();
+            //Dictionary<string, object> _cache = null;
+            List<string> groupsPre = null;
+            NodesBatches _cacheBatches = null;
 
-            for (int i = 0; i < generationsTree.Count; i++)
+            foreach (var generation in generationsTree)
             {
-                var generation = generationsTree[i];
-                var allJsonLevel = new Dictionary<string, object>();
-                foreach (var group in generation.ToList())
+                foreach (var group in generation.NodeGroups)
                 {
                     var jsonLevel = new Dictionary<string, object>();
-                    foreach (var node in group.Nodes)
+                    foreach (var node in group)
                     {
                         jsonLevel[node.Key] = GetTypedValue(node.Value, node.ValueKind);
                     }
-                    allJsonLevel[group.GroupName] = jsonLevel;
+                    if (_cacheBatches != null)
+                    {
+                        //_cache[generation.Header.GroupName] = jsonLevel;
+                        //_cacheBatches.Nodes.Where(x=>x.)
+                        topJson[generation.Header.GroupName] = jsonLevel;
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(generation.Header.GroupName))
+                        {
+                            foreach (var item in jsonLevel)
+                            {
+                                topJson[item.Key] = item.Value;
+                            }
+                        }
+                        else
+                            topJson[generation.Header.GroupName] = jsonLevel;
+                        //_cache = jsonLevel;
+                    }
+
                 }
-                topJson[i.ToString()] = allJsonLevel;
+                //_cacheBatches = generation;
+
             }
 
             var json = JsonSerializer.Serialize(topJson);
@@ -255,5 +317,16 @@ namespace JsonDataTreeVisualizer.Pages
             }
             return value;
         }
+    }
+
+    class NodesBatches
+    {
+        public GroupHeader Header { get; set; }
+        public List<List<SimpleDataNode>> NodeGroups { get; set; }
+    }
+
+    class NodesByParent : List<SimpleDataNode>
+    {
+        //public List<SimpleDataNode> Nodes { get; set; }
     }
 }
