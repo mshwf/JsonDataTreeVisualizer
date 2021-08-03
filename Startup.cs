e4de5c6aa6491c930a6438563b7983adb439c2fb
Mohamed Elshawaf <m_shawaf@outlook.com>
2021-08-03 23:19:01 +0200

    using razor component

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,6 @@ namespace JsonDataTreeVisualizer
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
